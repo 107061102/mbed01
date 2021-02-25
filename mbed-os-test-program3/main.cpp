d8f7cc7559804ff522fe3e256847f541a10fbfd7
107061102 <george890823@gmail.com>
2021-02-25 03:41:05 -0800

    Add main.cpp

---
+++ mbed-os-test-program3/main.cpp
@@ -0,0 +1,21 @@
+#include "mbed.h"
+
+DigitalOut myLED(LED1);
+DigitalOut myLED2(LED3);
+
+
+int main()
+{
+    myLED = 0;
+    myLED2 = 1;
+    while (true){
+        for (int i = 0; i < 6; ++i){                     //blink for 10 times
+            myLED2 = !myLED2; // toggle led
+            ThisThread::sleep_for(500ms);
+        }
+        for (int i = 0; i < 4; ++i){                     //blink for 10 times
+            myLED = !myLED; // toggle led
+            ThisThread::sleep_for(500ms);
+        }
+    }
+}
